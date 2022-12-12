e033db7e2fd56d7ab03bba88b6428ba397b11702
monchinawat <monchinawat@gmail.com>
2022-12-12 22:26:52 +0700

    Implement get travel node

--- dijkstra.py
+++ dijkstra.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List
 
 import numpy as np
 
@@ -11,30 +11,37 @@ DIJKSTRA_CONFIG = {
     "target": "F",
 }
 
+
 def get_weight(path_length: Dict[float, str], key: str) -> float:
     return path_length.get(key, DEFAULT_VALUE)[0]
 
+
+def get_travel_node(order_path: List):
+    travel_node = [(order_path[i], order_path[i + 1]) for i in range(len(order_path) - 1)]
+    return travel_node
+
+
 graph_array = np.array(
     [
-        ["A", "B", 4],
-        ["A", "C", 5],
-        ["B", "C", 11],
-        ["B", "D", 9],
-        ["B", "E", 7],
-        ["C", "E", 3],
-        ["D", "E", 13],
-        ["D", "F", 2],
-        ["E", "F", 6],
+        ("A", "B", 4),
+        ("A", "C", 5),
+        ("B", "C", 11),
+        ("B", "D", 9),
+        ("B", "E", 7),
+        ("C", "E", 3),
+        ("D", "E", 13),
+        ("D", "F", 2),
+        ("E", "F", 6),
     ]
 )
 graph = UndirectedGraph.from_numpy(array=graph_array, weighted_graph=True)
 unupdate_path = Tree.vertices2name(graph.vertices)
+path_length = {
+    DIJKSTRA_CONFIG["start"]: [0, DIJKSTRA_CONFIG["start"]],
+}
+
 start = DIJKSTRA_CONFIG["start"]
 target = DIJKSTRA_CONFIG["target"]
-
-path_length = {
-    start: [0, start],
-}
 
 while len(unupdate_path):
     current_vertex = min(unupdate_path, key=lambda x: get_weight(path_length, x))
@@ -52,9 +59,12 @@ while len(unupdate_path):
         path_length[adj] = [new_path, current_vertex]
     unupdate_path.remove(current_vertex)
 
-shortest_path = list(DIJKSTRA_CONFIG.values())
+shortest_path = [start]
 current_node = target
 while current_node != start:
     shortest_path.insert(1, current_node)
     current_node = path_length[current_node][1]
+
+
 print(shortest_path, smallest_weight := path_length[target][0])
+print(get_travel_node(shortest_path))

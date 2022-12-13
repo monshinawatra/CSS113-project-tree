a9d6c489cd4917de1cd10fb1121a42a8fd105791
ChothaninThitisrirat <111549219+ChothaninThitisrirat@users.noreply.github.com>
2022-12-13 09:00:31 +0700

    BFS

--- BFS.py
+++ BFS.py
@@ -1,6 +1,28 @@
 import numpy as np
 from tree.graph import UndirectedGraph
 
+def bfs(first:str,graph):
+    queue = []
+    path = []
+    result = []
+    queue.append(first)     # add the start node to queue
+    path.append(first)      # add the start node to path
+    
+    while queue != []:      # loop until queue empty
+        adj = UndirectedGraph.vertices2name(graph.get_adjacent_vertices(queue[0]))  # get adjacent vertices in queue[0]
+        while adj != []:                            # loop until no adjacent vertices in queue[0]
+            for i in adj:   
+                if i not in path:                   # if adjacent vertices not in path then 
+                    queue.append(i)                 # add to queue
+                    path.append(i)                  # add to path
+                    result.append((queue[0], i))    # add to result as tuple from queue[0] to adj[i]
+                    adj.remove(i)                   # remove from adj
+                else:
+                    adj.remove(i)                   # remove from adj if in path already
+        queue.remove(queue[0])                      # remove from queue
+    return result                                   # return result 
+
+
 graph_array = np.array(
     [
         ("A", "B", 4),
@@ -14,28 +36,9 @@ graph_array = np.array(
         ("E", "F", 6),
     ]
 )
-
 graph = UndirectedGraph.from_numpy(array=graph_array, weighted_graph=False)
-bfs_config = {"first": "A",} 
-all_vertex = [vertex.name for vertex in graph.vertices]
-queue = []
-path = []
-queue.append(all_vertex[0])
-adj = UndirectedGraph.vertices2name(graph.get_adjacent_vertices(queue[0]))
-path.append(all_vertex[0])
-while queue != []:
-    adj = UndirectedGraph.vertices2name(graph.get_adjacent_vertices(queue[0]))
-    while adj != []:
-        for i in adj:
-            if i not in path:
-                queue.append(i)
-                path.append(i)
-                adj.remove(i)
-            else:
-                adj.remove(i)
-    queue.remove(queue[0]) 
 
-print(path)       
+print(bfs("F", graph))       
 
 
 
